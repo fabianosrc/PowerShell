61b43e9e5d7b2556bd3124bfd7593c39f45ac1df
Fabiano M. Silva <fabiano.msrc@outlook.com>
2024-01-03 13:45:01 -0300

    Script to rename part of file from directory

---
+++ Scripts/Replace-Part-Of-Filename.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+    Rename part of fileName in directory
+.DESCRIPTION
+    Renames parts of a list of files from a directory
+.EXAMPLE
+    PS C:\> .\Replace-Part-Of-Filename.ps1
+.LINK
+    https://github.com/fabianosrc/PowerShell
+#>
+
+$path = ''
+$partial = ''
+$replace = ''
+
+if (! (Test-Path -Path $path)) {
+    Write-Host 'Directory path is invalid or not defined' -ForegroundColor Yellow
+} else {
+    Get-ChildItem -Path $path -Filter '*.ini' -Recurse | ForEach-Object {
+        Rename-Item -Path $_.FullName -NewName $_.Name.Replace($partial, $replace)
+    }
+}
